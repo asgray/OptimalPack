d081b10aef95d8a0eaaf72b2cdd8be313bfa51fc
asgray <antgray89@gmail.com>
2020-05-07 22:03:54 -0700

    implemented react-table

---
+++ Old_Implementation/components/customtable.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import TableRow from "./tablerow";
+import logo from "./logo.svg";
+import { useHTTP } from "../hooks/http";
+
+function Table({ url, id }) {
+  // url is the route to fetch, id is the column to use as the table index
+  // collect data from the API
+  const [isLoaded, res] = useHTTP(url);
+  var sql = {};
+  if (isLoaded) {
+    const headers = res.data.headers;
+    const data = res.data.data;
+    sql = { headers, data };
+  }
+
+  function renderWaiting() {
+    return <img src={logo} className="App-logo" alt="logo" />;
+  }
+
+  function renderTable() {
+    return (
+      <>
+        <h1>{url}</h1>
+        <table>
+          <thead>
+            <tr key={url + "head"}>
+              {/* Render column headers */}
+              {sql.headers.map((inst) => {
+                //   exclude indexing value
+                if (inst !== id) {
+                  return (
+                    <th key={inst}>
+                      {/* capitalize values in header */}
+                      {inst.charAt(0).toUpperCase() + inst.slice(1)}
+                    </th>
+                  );
+                } else {
+                  return null;
+                }
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {
+              // render each row as it's own element
+              sql.data.map((inst) => {
+                return (
+                  <TableRow
+                    key={id + inst[id]}
+                    data={inst}
+                    id={id}
+                    colorder={sql.headers}
+                  />
+                );
+              })
+            }
+          </tbody>
+        </table>
+      </>
+    );
+  }
+
+  if (isLoaded) {
+    return renderTable();
+  } else {
+    return renderWaiting();
+  }
+}
+export default React.memo(Table);
---
+++ Old_Implementation/components/customtablerow.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function TableRow({ data, id, colorder }) {
+  // data contains the data for each row as a dictionary, id is the table index column
+  // colorder is a list of column headers in table order
+  // find the element that will serve as the row key
+  let key = data[id];
+  return (
+    // assign row key
+    <tr key={key}>
+      {colorder.map((col) => {
+        // add elements to the row in column order
+        // exclude id column
+        if (col !== id) {
+          let content;
+          // change content if cooked or not
+          if (col === "cooked") {
+            if (data[col] === 1) {
+              content = "yes";
+            } else {
+              content = "no";
+            }
+          } else {
+            // otherwise render item data
+            content = data[col];
+          }
+          return <td key={col.toString() + data[col].toString()}>{content}</td>;
+        } else {
+          return null;
+        }
+      })}
+    </tr>
+  );
+}
+
+export default TableRow;
