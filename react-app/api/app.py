f41431bf5c4806755e4762185dd9dcccb2521e50
asgray <antgray89@gmail.com>
2020-05-12 19:39:56 -0700

    added sqlalchemy automap

--- react-app/api/app.py
+++ react-app/api/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
 from flask_cors import CORS
 import pymysql
 pymysql.install_as_MySQLdb()
@@ -10,41 +11,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost:3306/hi
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 CORS(app)
 db = SQLAlchemy(app)
+Base = automap_base()
+Base.prepare(db.engine, reflect=True)
 
-
-class Food(db.Model):
-    __tablename__ = 'food'
-    idfood = db.Column('idfood', db.Integer, primary_key=True)
-    name = db.Column('name', db.String(45))
-    brand = db.Column('brand', db.String(45))
-    weight = db.Column('weight', db.Integer)
-    calories = db.Column('calories', db.Integer)
-    protein = db.Column('protein', db.Integer)
-    servings = db.Column('servings', db.Integer)
-    cooked = db.Column('cooked', db.Boolean)
-    headers = [ 'idfood', 'name', 'brand', 'weight', 'calories', 'protein', 'servings', 'cooked']
-
-    def __init__(self, name, brand, weight, calories, protein, servings, cooked):
-        # self.idfood = idfood
-        self.name = name
-        self.brand = brand
-        self.weight = weight
-        self.calories = calories
-        self.protein = protein
-        self.servings = servings
-        self.cooked = cooked
-
-    def __repr__(self):
-        return f'{{{self.idfood} {self.name} {self.brand} {self.weight} {self.calories} {self.protein} {self.servings} {self.cooked}}}'
+Food = Base.classes.food
 
 
 @app.route('/food')
 def food():
-    headers = Food.headers
     foods = [{'idfood':food.idfood, 'name': food.name, 'brand': food.brand, 'weight': food.weight,
             'calories': food.calories, 'protein': food.protein, 'servings': food.servings,
-            'cooked': int(food.cooked)} for food in Food.query.all()]
-    return jsonify({'headers': headers, 'data':foods})
+            'cooked': int(food.cooked)} for food in db.session.query(Food).all()]
+    return jsonify({'data':foods})
 
 
 @app.route('/food_insert', methods=['POST'])
